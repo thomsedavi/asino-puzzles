db5a9db10dee9013e98d51b5b1f63a96542992fc
David Thomsen <uniquelydave@gmail.com>
2023-06-11 16:03:44 +1200

    Ensure SVG layers always show something

--- src/asino/svg/Svg.tsx
+++ src/asino/svg/Svg.tsx
@@ -10,31 +10,35 @@ export const drawSvg = (puzzle: AsinoPuzzle): JSX.Element => {
               viewBox='0 0 5040 5040'
               xmlns="http://www.w3.org/2000/svg">
     {puzzle.layers?.map((layer: AsinoLayer, layerIndex: number) => {
+      var result = <text key={`layer${layerIndex}`}>Error</text>
+
       if (layer.lineId !== undefined) {
         const layerLine = puzzle.lines?.filter(line => line.id === layer.lineId)[0];
 
         if (layerLine !== undefined) {
-          return drawLine(layerLine, puzzle, layerIndex);
+          result = drawLine(layerLine, puzzle, layerIndex);
         }
       } else if (layer.interfaceId !== undefined) {
         const layerInterface = puzzle.interfaces?.filter(asinoInterface => asinoInterface.id === layer.interfaceId)[0];
 
         if (layerInterface !== undefined) {
-          return drawInterface(layerInterface, puzzle, layerIndex);
+          result = drawInterface(layerInterface, puzzle, layerIndex);
         }
       } else if (layer.circleId !== undefined) {
         const layerCircle = puzzle.circles?.filter(circle => circle.id === layer.circleId)[0];
 
         if (layerCircle !== undefined) {
-          return drawCircle(layerCircle, puzzle, layerIndex);
+          result = drawCircle(layerCircle, puzzle, layerIndex);
         }
       } else if (layer.rectangleId !== undefined) {
         const layerRectangle = puzzle.rectangles?.filter(rectangle => rectangle.id === layer.rectangleId)[0];
 
         if (layerRectangle !== undefined) {
-          return drawRectangle(layerRectangle, puzzle, layerIndex);
+          result = drawRectangle(layerRectangle, puzzle, layerIndex);
         }
       }
+
+      return result;
     })}
   </svg>;
 }
