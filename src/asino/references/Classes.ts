6c5beab73a530579839c9bc33720a070ada85e32
David Thomsen <uniquelydave@gmail.com>
2023-08-27 09:47:08 +1200

    Add remainder of curves

--- src/asino/references/Classes.ts
+++ src/asino/references/Classes.ts
@@ -2,12 +2,109 @@ import { AsinoClassReference } from "../types/Class";
 
 export const systemClassDefaults: AsinoClassReference[] = [
   {
+    id: 'e-af',
+    name: { value: 'Curve Right To Bottom And Left To Top' },
+    value: {
+      layers: [
+        {
+          path: { id: 'f-ee' }
+        },
+        {
+          path: { id: 'f-eb' }
+        }
+      ]
+    }
+  },
+  {
+    id: 'b-dd',
+    name: { value: 'Curve Right To Bottom' },
+    value: {
+      layers: [
+        {
+          path: { id: 'f-ee' }
+        }
+      ]
+    }
+  },
+  {
+    id: 'e-be',
+    name: { value: 'Curve Left To Top' },
+    value: {
+      layers: [
+        {
+          path: { id: 'f-eb' }
+        }
+      ]
+    }
+  },
+  {
+    id: 'd-ab',
+    name: { value: 'Curve Top To Right And Bottom To Left' },
+    value: {
+      layers: [
+        {
+          path: { id: 'b-ff' }
+        },
+        {
+          path: { id: 'b-bc' }
+        }
+      ]
+    }
+  },
+  {
+    id: 'e-cd',
+    name: { value: 'Curve Top To Bottom And Right To Left' },
+    value: {
+      layers: [
+        {
+          path: { id: 'c-fb' }
+        },
+        {
+          path: { id: 'a-ee' }
+        }
+      ]
+    }
+  },
+  {
+    id: 'e-da',
+    name: { value: 'Curve Right To Left' },
+    value: {
+      layers: [
+        {
+          path: { id: 'c-fb' }
+        }
+      ]
+    }
+  },
+  {
+    id: 'a-cf',
+    name: { value: 'Curve Top To Right' },
+    value: {
+      layers: [
+        {
+          path: { id: 'b-ff' }
+        }
+      ]
+    }
+  },
+  {
     id: 'a-df',
     name: { value: 'Curve Bottom To Left' },
     value: {
       layers: [
         {
           path: { id: 'b-bc' }
+        }
+      ]
+    }
+  },
+  {
+    id: 'd-ce',
+    name: { value: 'Curve Top To Bottom' },
+    value: {
+      layers: [
+        {
+          path: { id: 'a-ee' }
         }
       ]
     }
--- src/asino/references/Paths.ts
+++ src/asino/references/Paths.ts
@@ -3,6 +3,826 @@ import { AsinoPathReference } from "../types/Path";
 
 export const systemPathDefaults: AsinoPathReference[] = [
   {
+    id: 'f-ee',
+    name: { value: 'Curve Right To Bottom' },
+    value: {
+      commands: [
+        {
+          letter: 'M',
+          x: 1,
+          y: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'C',
+          x1: {
+            operator: '-',
+            numberInputs: [
+              1,
+              {
+                operator: '*',
+                numberInputs: [
+                  CurveFraction,
+                  {
+                    operator: '-',
+                    numberInputs: [
+                      { numerator: 1, denominator: 2 },
+                      {
+                        operator: '/',
+                        numberInputs: [
+                          PathSize,
+                          2
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          y1: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          x2: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y2: {
+            operator: '-',
+            numberInputs: [
+              1,
+              {
+                operator: '*',
+                numberInputs: [
+                  CurveFraction,
+                  {
+                    operator: '-',
+                    numberInputs: [
+                      { numerator: 1, denominator: 2 },
+                      {
+                        operator: '/',
+                        numberInputs: [
+                          PathSize,
+                          2
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          x: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 1
+        },
+        {
+          letter: 'L',
+          x: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 1
+        },
+        {
+          letter: 'C',
+          x1: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y1: {
+            operator: '-',
+            numberInputs: [
+              1,
+              {
+                operator: '*',
+                numberInputs: [
+                  CurveFraction,
+                  {
+                    operator: '+',
+                    numberInputs: [
+                      { numerator: 1, denominator: 2 },
+                      {
+                        operator: '/',
+                        numberInputs: [
+                          PathSize,
+                          2
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          x2: {
+            operator: '-',
+            numberInputs: [
+              1,
+              {
+                operator: '*',
+                numberInputs: [
+                  CurveFraction,
+                  {
+                    operator: '+',
+                    numberInputs: [
+                      { numerator: 1, denominator: 2 },
+                      {
+                        operator: '/',
+                        numberInputs: [
+                          PathSize,
+                          2
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          y2: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          x: 1,
+          y: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'Z'
+        }
+      ],
+      fill: Color
+    }
+  },
+  {
+    id: 'f-eb',
+    name: { value: 'Curve Left To Top' },
+    value: {
+      commands: [
+        {
+          letter: 'M',
+          x: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 0
+        },
+        {
+          letter: 'C',
+          x1: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y1: {
+            operator: '*',
+            numberInputs: [
+              CurveFraction,
+              {
+                operator: '+',
+                numberInputs: [
+                  { numerator: 1, denominator: 2 },
+                  {
+                    operator: '/',
+                    numberInputs: [
+                      PathSize,
+                      2
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          x2: {
+            operator: '*',
+            numberInputs: [
+              CurveFraction,
+              {
+                operator: '+',
+                numberInputs: [
+                  { numerator: 1, denominator: 2 },
+                  {
+                    operator: '/',
+                    numberInputs: [
+                      PathSize,
+                      2
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          y2: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          x: 0,
+          y: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'L',
+          x: 0,
+          y: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'C',
+          x1: {
+            operator: '*',
+            numberInputs: [
+              CurveFraction,
+              {
+                operator: '-',
+                numberInputs: [
+                  { numerator: 1, denominator: 2 },
+                  {
+                    operator: '/',
+                    numberInputs: [
+                      PathSize,
+                      2
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          y1: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          x2: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y2: {
+            operator: '*',
+            numberInputs: [
+              CurveFraction,
+              {
+                operator: '-',
+                numberInputs: [
+                  { numerator: 1, denominator: 2 },
+                  {
+                    operator: '/',
+                    numberInputs: [
+                      PathSize,
+                      2
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          x: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 0
+        },
+        {
+          letter: 'Z'
+        }
+      ],
+      fill: Color
+    }
+  },
+  {
+    id: 'b-ff',
+    name: { value: 'Curve Top To Right' },
+    value: {
+      commands: [
+        {
+          letter: 'M',
+          x: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 0
+        },
+        {
+          letter: 'C',
+          x1: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y1: {
+            operator: '*',
+            numberInputs: [
+              CurveFraction,
+              {
+                operator: '-',
+                numberInputs: [
+                  { numerator: 1, denominator: 2 },
+                  {
+                    operator: '/',
+                    numberInputs: [
+                      PathSize,
+                      2
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          x2: {
+            operator: '-',
+            numberInputs: [
+              1,
+              {
+                operator: '*',
+                numberInputs: [
+                  CurveFraction,
+                  {
+                    operator: '-',
+                    numberInputs: [
+                      { numerator: 1, denominator: 2 },
+                      {
+                        operator: '/',
+                        numberInputs: [
+                          PathSize,
+                          2
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          y2: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          x: 1,
+          y: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'L',
+          x: 1,
+          y: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'C',
+          x1: {
+            operator: '-',
+            numberInputs: [
+              1,
+              {
+                operator: '*',
+                numberInputs: [
+                  CurveFraction,
+                  {
+                    operator: '+',
+                    numberInputs: [
+                      { numerator: 1, denominator: 2 },
+                      {
+                        operator: '/',
+                        numberInputs: [
+                          PathSize,
+                          2
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          y1: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          x2: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y2: {
+            operator: '*',
+            numberInputs: [
+              CurveFraction,
+              {
+                operator: '+',
+                numberInputs: [
+                  { numerator: 1, denominator: 2 },
+                  {
+                    operator: '/',
+                    numberInputs: [
+                      PathSize,
+                      2
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          x: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 0
+        },
+        {
+          letter: 'Z'
+        }
+      ],
+      fill: Color
+    }
+  },
+  {
+    id: 'c-fb',
+    name: { value: 'Curve Right To Left' },
+    value: {
+      commands: [
+        {
+          letter: 'M',
+          x: 0,
+          y: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'L',
+          x: 0,
+          y: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'L',
+          x: 1,
+          y: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'L',
+          x: 1,
+          y: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          }
+        },
+        {
+          letter: 'Z'
+        }
+      ],
+      fill: Color
+    }
+  },
+  {
+    id: 'a-ee',
+    name: { value: 'Curve Top To Bottom' },
+    value: {
+      commands: [
+        {
+          letter: 'M',
+          x: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 0
+        },
+        {
+          letter: 'L',
+          x: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 0
+        },
+        {
+          letter: 'L',
+          x: {
+            operator: '+',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 1
+        },
+        {
+          letter: 'L',
+          x: {
+            operator: '-',
+            numberInputs: [
+              { numerator: 1, denominator: 2 },
+              {
+                operator: '/',
+                numberInputs: [
+                  PathSize,
+                  2
+                ]
+              }
+            ]
+          },
+          y: 1
+        },
+        {
+          letter: 'Z'
+        }
+      ],
+      fill: Color
+    }
+  },
+  {
     id: 'b-bc',
     name: { value: 'Curve Bottom To Left' },
     value: {
